872f548e161db4ebdb99bb41bc396998fcd12dfe
Marek Maciejewski <mmaciejewski@altimetrik.com>
2026-08-07 15:28:14 +0200

    init

---
+++ src/main/java/pl/mm/discountcoupons/DiscountCouponsManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.mm.discountcoupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCouponsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountCouponsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mm/discountcoupons/DiscountCouponsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mm.discountcoupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountCouponsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
